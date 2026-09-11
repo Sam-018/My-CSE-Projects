348327c2fb010065c4400d62ee5953d3c33ad135
Sameer Verma <vermasameer8340@gmail.com>
2026-09-11 11:36:56 +0530

    Added temperature converter

---
+++ Temperature Convertor.py
@@ -0,0 +1,18 @@
+print("Temperature Converter")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = int(input("Choose an option (1 or 2): "))
+
+temperature = float(input("Enter temperature: "))
+
+if choice == 1:
+    fahrenheit = (temperature * 9/5) + 32
+    print("Temperature in Fahrenheit:", fahrenheit)
+
+elif choice == 2:
+    celsius = (temperature - 32) * 5/9
+    print("Temperature in Celsius:", celsius)
+
+else:
+    print("Invalid choice")
